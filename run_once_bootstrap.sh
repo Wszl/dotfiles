575f1d3aafe86dacbb5b5c36144f11f1d00b0e04
Wszl <19342733+Wszl@users.noreply.github.com>
2024-08-30 11:21:58 +0800

    update

--- run_once_bootstrap.sh
+++ run_once_bootstrap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-os_code_name=$(lsb_release -cs)
+os_code_name=$(cat /etc/lsb-release | awk -F '=' 'NR==2 { print $2 }')
 font_name="Ubuntu Mono"
 
 # mirror
@@ -18,6 +17,8 @@ NVM_NODEJS_ORG_MIRROR=https://mirrors.ustc.edu.cn/node/
 NODE_MIRROR=https://mirrors.ustc.edu.cn/node/
 
 # font
+if test -z "$(whitch fc-list)"; then
+    sudo apt install -y -qq fontconfig
 if test -z "$(fc-list | grep $font_name)"; then
     font_version="$(curl https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep -E 'tag_name\": \"v[0-9]+\.[0-9]+\.[0-9]+' -o |head -n 1| tr -d 'tag_name\": \"')"
     wget -O /tmp/ubuntu_mono.tar.xz https://github.com/ryanoasis/nerd-fonts/releases/download/$font_version/UbuntuMono.tar.xz
@@ -28,13 +29,16 @@ if test -z "$(fc-list | grep $font_name)"; then
     cp /tmp/ubuntu_mono/* ~/.fonts
     fc-cache -f
 fi
+echo "font have been config."
 # fishshell
-sudo apt-get install fish
+sudo apt-get install -y -qq fish
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish 
+echo "fish shell have been installed."
 # alacritty
 sudo add-apt-repository ppa:system76/pop
 sudo sed -i 's/https:\/\/ppa.launchpadcontent.net/https:\/\/launchpad.proxy.ustclug.org/g' /etc/apt/sources.list.d/system76-ubuntu-pop-$os_code_name.list
 sudo apt-get update
-sudo apt-get install alacritty
+sudo apt-get install -y -qq alacritty
+echo "alacritty have been installed."
 
